01192a6fbff8bde72a2160de130144a6091c6489
JulieDuong <duong.julie@webisoft.com>
2020-05-22 03:21:50 -0400

    Updates

---
+++ precache-manifest.2b46761b7a7bd1d61b9f329d0e25342f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe154557bc346b458b2e90e2279322ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f1aa2002ccb5d46f8a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c0fb1e04582b0a6ad5d",
+    "url": "/static/js/2.10d26316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10d26316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f1aa2002ccb5d46f8a",
+    "url": "/static/js/main.5150ac70.chunk.js"
+  },
+  {
+    "revision": "43a8c5900210fd36157e",
+    "url": "/static/js/runtime-main.0d570784.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
